76c9bfdf7924550bb73802af1e683f0fd46829ea
zhangchao07188 <zhangchao07188@126.com>
2015-06-02 14:36:37 +0800

    首次提交到本地仓库

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author james
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
